0d5753dcbcf88268e33a89f529be8854e83d13ab
Manasvi Mohan Sharma <manasvimsharma@gmail.com>
2022-11-11 09:58:00 +0530

    Made usual changes

--- push-to-github.sh
+++ push-to-github.sh
@@ -1,3 +1,8 @@
+#hint:   git config pull.rebase false  # merge (the default strategy)
+#hint:   git config pull.rebase true   # rebase
+#hint:   git config pull.ff only       # fast-forward only
+
+# git pull
 git add --all
 git commit -m "Made usual changes"
 git push
